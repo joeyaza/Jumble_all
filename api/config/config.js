533490d029b00db11b6ff5d51b2283c1192ab922
stain88 <marcbaghdadi@gmail.com>
2015-11-23 10:37:58 +0000

    Touched models, updated database notes

--- api/config/config.js
+++ api/config/config.js
@@ -1,4 +1,4 @@
-var mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/project-three';
+var mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/jumbulIt';
 module.exports = {
   'secret': process.env.PROJECT_THREE_SECRET_KEY,
   'database': mongoUri
