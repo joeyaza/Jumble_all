4a5a91522917b10bd21835a8d9ff65e711973035
stain88 <marcbaghdadi@gmail.com>
2015-11-25 19:25:13 +0000

    profile page cleanup

--- front-end/scripts/main.js
+++ front-end/scripts/main.js
@@ -2,12 +2,12 @@ $( init);
 
 function init(){
   $('#fullpage').fullpage({
-    sectionsColor: ['#A5D8FF', '#758ECD', '#D5C6E0', '#C5979D', '#C5979D'],
+    sectionsColor: ['#A5D8FF', '#758ECD', '#D5C6E0', '#C5979D'],
     fixedElements: '#navbar',
     anchors: ['landingPage', 'about', 'madeBy']
   });
   $("form").on("submit", submitForm);
-  $("#profile").on("click", Profile);
+  $(".profile-link").on("click", getProfile);
   $(".logout-link").on("click", logout);
   $(".login-link, .register-link").on("click", showPage);
   $('.modal-trigger').leanModal();
@@ -99,9 +99,9 @@ function authenticationSuccessful(data) {
 function setData(data) {
   localStorage.setItem("token", data.token);
   localStorage.setItem("userId", data.user._id);
-  localStorage.setItem("user-name", data.user.username);
-  localStorage.setItem("user-email", data.user.email);
-  localStorage.setItem("user-pic", data.user.picture_url);
+  localStorage.setItem("user-name", data.user.local.username);
+  localStorage.setItem("user-email", data.user.local.email);
+  localStorage.setItem("user-pic", data.user.local.picture_url);
 }
 
 function getToken() {
@@ -130,20 +130,15 @@ function ajaxRequest(method, url, data, callback) {
   });
 }
 
-function Profile(data){
+function getProfile(event){
   event.preventDefault();
   var userId = localStorage.getItem("userId");
-  return ajaxRequest("get", "http://localhost:3000/api/users/:" + userId, data, showProfile)
+  return ajaxRequest("get", "http://localhost:3000/api/users/" + userId, null, showProfile)
 }
 
 function showProfile(data){
   event.preventDefault();
   console.log(data)
-
-  data.users.forEach(function(user) {
-    // $(".users").append( '<li>' + user.local.email + '</li>' )
-  })
-
 }
 
 function hideProfile(){
